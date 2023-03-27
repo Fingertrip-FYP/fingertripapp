0d7a0fdc31016251d4e444af9621952f4f59bd6e
CryptoLeo-3000 <padavekartik@gmail.com>
2023-03-27 22:06:27 +0530

    Sign In page

--- pages/SignIn.js
+++ pages/SignIn.js
@@ -1,67 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, ImageBackground, StyleSheet, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
 
 function SignInScreen() {
-  const [userID, setUserID] = useState('');
-  const [password, setPassword] = useState('');
-  const navigation = useNavigation();
-  const express = require('express');
-  const mysql = require('mysql');
-  const bodyParser = require('body-parser');
-  const app = express();
-
-  const connection = mysql.createConnection({
-    host: '127.0.0.1:3306',
-    user: 'root',
-    password: 'ShinigamiLeo3000@',
-    database: 'fingertrip',
-  });
-
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-
-  app.post('/signin', (req, res) => {
-    const { email, password } = req.body;
-    const query = `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`;
-
-    connection.query(query, (err, results, fields) => {
-      if (err) throw err;
-
-      if (results.length > 0) {
-        res.status(200).json({ message: 'Success' });
-      } else {
-        res.status(401).json({ message: 'Invalid credentials' });
-      }
-    });
-  });
-
-  app.listen(3306, () => {
-    console.log('Server started on port 3000');
-  });
-
-
-  const handleSignIn = () => {
-    const formData = new FormData();
-    formData.append('email', email);
-    formData.append('password', password);
-
-    fetch('http://127.0.0.1:3306/signin', {
-      method: 'POST',
-      body: formData,
-    })
-      .then((response) => {
-        if (response.ok) {
-          navigation.navigate('Welcome');
-        } else {
-          Alert.alert('Invalid credentials');
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
   return (
     <ImageBackground source={require('../assets/signin-bg2.png')} style={styles.background}>
       <View style={styles.container}>
