7195e509244f2ba1f2f70a105c3aedcb534ce244
CryptoLeo-3000 <padavekartik@gmail.com>
2023-03-27 22:06:07 +0530

    App files

--- databases/insert_values_script.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO logins (user_id, password, guest_name) VALUES ('70362019039', 'Pass$2019@', 'Kartik Padave');
-INSERT INTO logins (user_id, password, guest_name) VALUES ('70362019031', 'Pass$2019@', 'Simran Kumari');
-INSERT INTO logins (user_id, password, guest_name) VALUES ('70362019028', 'Pass$2019@', 'Varun Khadayate');
-
-SELECT * FROM logins;
