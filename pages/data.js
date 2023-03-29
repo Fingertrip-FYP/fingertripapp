4703cd7a10c22389d2c26079015bf4cb6a91f754
CryptoLeo-3000 <padavekartik@gmail.com>
2023-03-30 01:16:22 +0530

    Image data for carousel

---
+++ pages/data.js
@@ -0,0 +1,16 @@
+const data = [
+    {
+      imgUrl: "../assets/slideshow1.png",
+    },
+    {
+      imgUrl: "../assets/slideshow2.png",
+    },
+    {
+      imgUrl: "../assets/slideshow3.png",
+    },
+    {
+      imgUrl: "../assets/slideshow4.png",
+    },
+  ];
+  
+  export default data;
