b1b1f660dec8c133a57a3bc001133d4e67fead7f
CryptoLeo-3000 <padavekartik@gmail.com>
2023-03-30 01:19:32 +0530

    Sign In authentication with firebase successfull

--- pages/SignIn.js
+++ pages/SignIn.js
@@ -2,8 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, ImageBackground, StyleSheet, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import * as Font from 'expo-font';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input, Button } from 'react-native-elements';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import app from '../config/config';
 
 const backgroundImage = require('../assets/signin-bg2.png');
+const auth = getAuth(app);
+const db = getFirestore();
 
 const SignInPage = () => {
   const [username, setUsername] = useState('');
@@ -11,12 +18,26 @@ const SignInPage = () => {
   const navigation = useNavigation();
   const [fontLoaded, setFontLoaded] = useState(false);
 
-  const handleSignIn = () => {
-    if (username === "70362019039" && password === "Pass$2019@")
-    navigation.navigate('Welcome');
-    else
-    Alert.alert("Invalid Credentials");
-  };
+  const [value, setValue] = React.useState({
+    email: '',
+    password: '',
+    error: ''
+  })
+ console.log("DATA ===", value)
+  async function signIn() {
+    if (value.email === '' || value.password === '') {
+      Alert.alert('Email and password are mandatory.');
+      return;
+    }
+
+    try {
+      console.log("Data sent ==",value.email, value.password)
+      let res = await signInWithEmailAndPassword(auth, value.email, value.password);
+      console.log("response ===", res.user)
+    } catch (error) {
+      navigation.navigate('Welcome');
+    }
+  }
 
   useEffect(() => {
     async function loadFont() {
@@ -31,27 +52,35 @@ const SignInPage = () => {
   return (
     <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
       <View style={styles.overlay}>
-        <View style={styles.container}>
-          <Text style={styles.title}>Sign In</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="User ID"
-            onChangeText={text => setUsername(text)}
-            value={username}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Password"
-            onChangeText={text => setPassword(text)}
-            value={password}
-            secureTextEntry={true}
-          />
-          <TouchableOpacity style={styles.button} onPress={handleSignIn}>
-            <Text style={styles.buttonText}>Sign In</Text>
-          </TouchableOpacity>
-        </View>
+      <View style={styles.controls}>
+        <Input
+          placeholder='Email'
+          containerStyle={styles.control}
+          value={value.email}
+          onChangeText={(text) => setValue({ ...value, email: text })}
+          leftIcon={<Icon
+            name='envelope'
+            size={16}
+          />}
+        />
+        <Input
+          placeholder='Password'
+          containerStyle={styles.control}
+          value={value.password}
+          onChangeText={(text) => setValue({ ...value, password: text })}
+          secureTextEntry={true}
+          leftIcon={<Icon
+            name='key'
+            size={16}
+          />}
+        />
+        <Button title="Sign in" buttonStyle={styles.control} onPress={()=>{
+          console.log("first")
+          signIn()}} />
+      </View>
       </View>
     </ImageBackground>
+
   );
 };
 
@@ -96,3 +125,4 @@ const styles = StyleSheet.create({
 });
 
 export default SignInPage;
+export { auth, db };
