357ff190cb204629a41eb619062978c87e9c29e5
CryptoLeo-3000 <padavekartik@gmail.com>
2023-04-04 18:31:49 +0530

    Configured firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration, replace it with your project keys
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "apiKey",
-  authDomain: "authDomain",
-  projectId: "react-auth-tutorial-1234",
-  storageBucket: "storageBucket",
-  messagingSenderId: "messagingSenderId",
-  appId: "appId"
+  apiKey: "AIzaSyDd5nYB-eo06XZuerHmEwSHOL96V00wyvA",
+  authDomain: "fingertrip-a8fe1.firebaseapp.com",
+  databaseURL: "https://fingertrip-a8fe1-default-rtdb.firebaseio.com",
+  projectId: "fingertrip-a8fe1",
+  storageBucket: "fingertrip-a8fe1.appspot.com",
+  messagingSenderId: "465093245343",
+  appId: "1:465093245343:web:a78ace7e5bf53ff2bea5aa",
+  measurementId: "G-C224NW70QQ"
 };
 
 // Initialize Firebase
