92fe77d955db4aedf146e715bd7c0753a2d8e922
CryptoLeo-3000 <padavekartik@gmail.com>
2023-05-08 19:46:05 +0530

    Till home page done

--- fingertripapp/App.js
+++ fingertripapp/App.js
@@ -1,7 +1,16 @@
-import React, {useEffect, useCallback} from 'react';
-import { View, TextInput, StyleSheet, ImageBackground, Button } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React, { useEffect, useState } from 'react';
+import { 
+  View, 
+  TextInput, 
+  StyleSheet, 
+  ImageBackground, 
+  Button, 
+  SafeAreaView, 
+  Text, 
+  FlatList 
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import * as i18n from './pages/i18n';
 // import LocalizationContext from './pages/localisationcontext';
 
@@ -10,7 +19,10 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+      screenOptions={{
+        headerLeft: null,
+      }}>
         {/* <Stack.Screen
           name="Language"
           component={Language}
@@ -18,7 +30,6 @@ export default function App() {
         <Stack.Screen
           name="SignIn"
           component={SignIn}
-          options={{title: 'signin'}}
         />
         <Stack.Screen
           name="Welcome"
@@ -27,6 +38,7 @@ export default function App() {
         <Stack.Screen
           name="Home"
           component={Home}
+          options={{headerLeft: null}}
         />
       </Stack.Navigator>
     </NavigationContainer>
@@ -124,33 +136,103 @@ function Welcome({navigation}) {
 
 // Home Screen
 function Home() {
-  this.state = {
-    username: '',
-    password: '',
+  // Search Bar
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setFilteredDataSource(responseJson);
+        setMasterDataSource(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    if (text) {
+      // Inserted text is not blank
+      // Filter the masterDataSource and update FilteredDataSource
+      const newData = masterDataSource.filter(function (item) {
+        // Applying filter for the inserted text in search bar
+        const itemData = item.title
+          ? item.title.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      // Inserted text is blank
+      // Update FilteredDataSource with masterDataSource
+      setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
   };
 
+  const ItemView = ({ item }) => {
+    return (
+      // Flat List Item
+      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+        {item.id}
+        {'.'}
+        {item.title.toUpperCase()}
+      </Text>
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#C8C8C8',
+        }}
+      />
+    );
+  };
+
+  const getItem = (item) => {
+    // Function for click on an item
+    alert('Id : ' + item.id + ' Title : ' + item.title);
+  };
+
+  // 3 main options
+
   return (
-    <View style={styles.signincontainer}>
-      <TextInput
-        value={this.state.username}
-        onChangeText={(username) => this.setState({ username })}
-        placeholder={'Username'}
-        style={styles.input}
-      />
-      <TextInput
-        value={this.state.password}
-        onChangeText={(password) => this.setState({ password })}
-        placeholder={'Password'}
-        secureTextEntry={true}
-        style={styles.input}
-      />
-      
-      <Button
-        title={'Login'}
-        style={styles.input}
-        onPress={() => navigation.navigate('Welcome')}
-      />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.searchbarcontainer}>
+        <TextInput
+          style={styles.searchbarinput}
+          onChangeText={(text) => searchFilterFunction(text)}
+          value={search}
+          underlineColorAndroid="transparent"
+          placeholder="Search Here"
+        />
+        <FlatList
+          data={filteredDataSource}
+          keyExtractor={(item, index) => index.toString()}
+          // ItemSeparatorComponent={ItemSeparatorView}
+          // renderItem={ItemView}
+        />
+      </View>
+      <View style={styles.mainoptions}>
+        <View style={[styles.box, {flex: 1, backgroundColor: 'red'}]} />
+        <View style={[styles.box, {flex: 1, backgroundColor: 'orange'}]} />
+        <View style={[styles.box, {flex: 1, backgroundColor: 'green'}]} />
+      </View>
+      <View style={styles.tagcontainer}>
+        <Text style={styles.tagline}>Luxury is in the details!!</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -169,5 +251,35 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'black',
     marginBottom: 10,
+  },
+  searchbarcontainer: {
+    marginTop: 30,
+    backgroundColor: 'white',
+  },
+  itemStyle: {
+    padding: 10,
+  },
+  searchbarinput: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 20,
+    margin: 5,
+    borderColor: '#009688',
+    backgroundColor: '#FFFFFF',
+  },
+  mainoptions: {
+    marginTop: 50,
+    flexDirection: 'row',
+    padding: 30,
+    justifyContent: 'space-evenly',
+    backgroundColor: 'black',
+  },
+  box: {
+    height: 150,
+  },
+  tagcontainer: {
+    padding: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
 });
