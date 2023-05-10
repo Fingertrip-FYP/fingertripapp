97f52ff0ab6f4315eb853a9b53d57d24ba4835d7
CryptoLeo-3000 <padavekartik@gmail.com>
2023-05-10 10:19:24 +0530

    STarting backend integration

--- fingertripapp/App.js
+++ fingertripapp/App.js
@@ -20,51 +20,56 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+function EmptyScreen() {
+  return <View />;
+}
+
+// Bottom Navbar
+function NavBar() {
+  return(
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} options={{ headerShown: false, }}/>
+      <Tab.Screen name="Food" component={Food} options={{ headerShown: false, }} />
+      <Tab.Screen name="Service" component={Services} options={{ headerShown: false, }} />
+      <Tab.Screen name="Explore" component={Explore} options={{ headerShown: false, }} />
+      <Tab.Screen name="User" component={EmptyScreen} options={{ headerShown: false, }} />
+    </Tab.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-      screenOptions={{
-        headerLeft: null,
-      }}>
+      <Stack.Navigator>
         {/* <Stack.Screen
           name="Language"
           component={Language}
         /> */}
         <Stack.Screen
+          options={{
+            headerShown: false,
+          }}
           name="SignIn"
           component={SignIn}
         />
         <Stack.Screen
+          options={{
+            headerShown: false,
+          }}
           name="Welcome"
           component={Welcome}
         />
         <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
+          options={{
+            headerShown: false,
+          }}
+          name="HomePage"
+          component={NavBar}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-function EmptyScreen() {
-  return <View />;
-}
-
-// // Bottom Navbar
-// function NavBar() {
-//   return(
-//     <Tab.Navigator>
-//       <Tab.Screen name="Home" component={Home} />
-//       <Tab.Screen name="Food" component={EmptyScreen} />
-//       <Tab.Screen name="Service" component={EmptyScreen} />
-//       <Tab.Screen name="Explore" component={EmptyScreen} />
-//       <Tab.Screen name="User" component={EmptyScreen} />
-//     </Tab.Navigator>
-//   );
-// }
 
 // SignIn Screen
 function SignIn({navigation}) {
@@ -104,7 +109,7 @@ function Welcome({navigation}) {
 
   useEffect(() => {
     const countTimer = setInterval(() => {
-      navigation.navigate('Home')
+      navigation.navigate('HomePage')
     }, 5000);
 
     return function cleanup() {
@@ -228,6 +233,7 @@ function Home({ navigation }) {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <View style={{height: 40, backgroundColor: 'red'}}></View>
       <ScrollView>
           {/* SearchBar */}
         <View style={styles.searchbarcontainer}>
@@ -266,6 +272,311 @@ function Home({ navigation }) {
     </SafeAreaView>
   );
 };
+
+// Food Screen
+function Food() {
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setFilteredDataSource(responseJson);
+        setMasterDataSource(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    if (text) {
+      // Inserted text is not blank
+      // Filter the masterDataSource and update FilteredDataSource
+      const newData = masterDataSource.filter(function (item) {
+        // Applying filter for the inserted text in search bar
+        const itemData = item.title
+          ? item.title.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      // Inserted text is blank
+      // Update FilteredDataSource with masterDataSource
+      setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
+  };
+
+  const ItemView = ({ item }) => {
+    return (
+      // Flat List Item
+      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+        {item.id}
+        {'.'}
+        {item.title.toUpperCase()}
+      </Text>
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#C8C8C8',
+        }}
+      />
+    );
+  };
+
+  const getItem = (item) => {
+    // Function for click on an item
+    alert('Id : ' + item.id + ' Title : ' + item.title);
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{height: 40, backgroundColor: 'red'}}></View>
+        {/* SearchBar */}
+        <View style={styles.searchbarcontainer}>
+          <TextInput
+            style={styles.searchbarinput}
+            onChangeText={(text) => searchFilterFunction(text)}
+            value={search}
+            underlineColorAndroid="transparent"
+            placeholder="Search Here"
+          />
+          <FlatList
+            data={filteredDataSource}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        </View>
+        <View>
+          <ScrollView>
+            <FlatList
+              data={filteredDataSource}
+              keyExtractor={(item, index) => index.toString()}
+              ItemSeparatorComponent={ItemSeparatorView}
+              renderItem={ItemView}
+            />  
+          </ScrollView>
+        </View>
+    </SafeAreaView>
+  );
+}
+
+// Services Screen
+function Services() {
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setFilteredDataSource(responseJson);
+        setMasterDataSource(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    if (text) {
+      // Inserted text is not blank
+      // Filter the masterDataSource and update FilteredDataSource
+      const newData = masterDataSource.filter(function (item) {
+        // Applying filter for the inserted text in search bar
+        const itemData = item.title
+          ? item.title.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      // Inserted text is blank
+      // Update FilteredDataSource with masterDataSource
+      setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
+  };
+
+  const ItemView = ({ item }) => {
+    return (
+      // Flat List Item
+      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+        {item.id}
+        {'.'}
+        {item.title.toUpperCase()}
+      </Text>
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#C8C8C8',
+        }}
+      />
+    );
+  };
+
+  const getItem = (item) => {
+    // Function for click on an item
+    alert('Id : ' + item.id + ' Title : ' + item.title);
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{height: 40, backgroundColor: 'red'}}></View>
+      {/* SearchBar */}
+      <View style={styles.searchbarcontainer}>
+        <TextInput
+          style={styles.searchbarinput}
+          onChangeText={(text) => searchFilterFunction(text)}
+          value={search}
+          underlineColorAndroid="transparent"
+          placeholder="Search Here"
+        />
+        <FlatList
+          data={filteredDataSource}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+      <View>
+        <ScrollView>
+        <View>
+          <FlatList
+            data={filteredDataSource}
+            keyExtractor={(item, index) => index.toString()}
+            ItemSeparatorComponent={ItemSeparatorView}
+            renderItem={ItemView}
+          />
+        </View>
+      </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+// Explore Screen
+function Explore() {
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setFilteredDataSource(responseJson);
+        setMasterDataSource(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    if (text) {
+      // Inserted text is not blank
+      // Filter the masterDataSource and update FilteredDataSource
+      const newData = masterDataSource.filter(function (item) {
+        // Applying filter for the inserted text in search bar
+        const itemData = item.title
+          ? item.title.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      // Inserted text is blank
+      // Update FilteredDataSource with masterDataSource
+      setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
+  };
+
+  const ItemView = ({ item }) => {
+    return (
+      // Flat List Item
+      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+        {item.id}
+        {'.'}
+        {item.title.toUpperCase()}
+      </Text>
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#C8C8C8',
+        }}
+      />
+    );
+  };
+
+  const getItem = (item) => {
+    // Function for click on an item
+    alert('Id : ' + item.id + ' Title : ' + item.title);
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{height: 40, backgroundColor: 'red'}}></View>
+        {/* SearchBar */}
+        <View style={styles.searchbarcontainer}>
+          <TextInput
+            style={styles.searchbarinput}
+            onChangeText={(text) => searchFilterFunction(text)}
+            value={search}
+            underlineColorAndroid="transparent"
+            placeholder="Search Here"
+          />
+          <FlatList
+            data={filteredDataSource}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        </View>
+        <View>
+          <ScrollView>
+            <FlatList
+              data={filteredDataSource}
+              keyExtractor={(item, index) => index.toString()}
+              ItemSeparatorComponent={ItemSeparatorView}
+              renderItem={ItemView}
+            />  
+        </ScrollView>
+        </View>
+    </SafeAreaView>
+  );
+}
 
 // Styles
 const styles = StyleSheet.create({
